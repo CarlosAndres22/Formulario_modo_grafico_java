4ee226ce60292b35427c7cc5548be0c34cbe8390
andres poveda <tizam15@hotmail.com>
2017-02-24 13:19:04 -0500

    Subiendo archivos iniciales

---
+++ src/modo_grafico_formulario/Modo_grafico_Formulario.java
@@ -0,0 +1,18 @@
+
+package modo_grafico_formulario;
+
+/**
+ *
+ * @author C.andres
+ */
+public class Modo_grafico_Formulario {
+
+   
+    public static void main(String[] args) {
+     Formulario form = new Formulario ();
+     form.setVisible(true);
+        
+        
+    }
+    
+}
